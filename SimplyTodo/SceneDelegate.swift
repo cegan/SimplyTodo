b6877882c82617ba76a81958003e434d297a3389
Casey Egan <c.egan@me.com>
2021-03-14 01:41:13 -0700

    Misc

--- SimplyTodo/SceneDelegate.swift
+++ SimplyTodo/SceneDelegate.swift
@@ -16,8 +16,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
           
             let window = UIWindow(windowScene: windowScene)
-    
-        
             window.rootViewController = UINavigationController(rootViewController: todoTableViewController()) // Your initial view controller.
             window.makeKeyAndVisible()
             self.window = window
